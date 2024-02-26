39600cbd294a076da2343dd6d20ad2c1300c3497
Công Minh <ndmc@192.168.2.17>
2024-02-27 01:46:56 +0700

    done

---
+++ src/models/mongodb.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+let connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Kết nối database thành công!');
+  } catch (error) {
+    console.error('Lỗi kết nối database:', error);
+  }
+};
+
+module.exports = connectDB;
---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import viewEngine from './config/viewEngine'
+import initWebrouter from './route/web'
+import connectDB from './models/mongodb'
+require('dotenv').config()
+let app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+viewEngine(app);
+initWebrouter(app);
+
+
+let port = process.env.PORT || 2222;
+app.listen( port ,async	()=> {
+	await connectDB()
+	console.log('Connect thành công tại localhost:'+ port)
+})
