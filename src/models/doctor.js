847f3f7f9f00ce0a494cf28d2afe8e57c6a8aaae
Công Minh <ndmc@192.168.2.17>
2024-02-28 03:03:41 +0700

    update

--- src/models/doctor.js
+++ src/models/doctor.js
@@ -3,12 +3,35 @@ import mongoose from 'mongoose';
 let DoctorSchema = new mongoose.Schema({
   name:{
     type: String,
-    require: true,
+    required: true,
   },
-    user:{
-      type:mongoose.Schema.Types.ObjectId,
-      ref:'User'
-  }
+  email:{
+    type: String,
+    required:true,
+  },
+  address:{
+    type: String,
+    required:true
+  },
+          specialization:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Specialization'
+          },
+  phone:{
+    type: String,
+    required: true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  nameuser:{
+    type:String
+  },
+          user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+          }
 });
 let Doctor = mongoose.model('Doctor', DoctorSchema);
 
