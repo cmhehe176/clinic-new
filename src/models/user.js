38360e841917dfe49ad2defd57a7cacbe2b8be77
Công Minh <ndmc@cmhehe176.local>
2024-02-27 15:21:49 +0700

    update

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
+  name:{
+    type: String,
+    require: true,
+  },
+  roleID:{
+    type:Number,
+    require:true
+  }
 });
-
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
