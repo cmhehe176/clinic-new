4936820b6c5f1e398a2f8f043b0e22907c1d3198
Công Minh <ndmc@cmhehe176.local>
2024-02-27 16:31:02 +0700

    update

--- src/models/doctor.js
+++ src/models/doctor.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
-
-const DoctorSchema = new mongoose.Schema({
+import Sequence from 'mongoose-sequence'
+let seq = new Sequence('doctor_id', mongoose.connection);
+    seq.create({})
+let DoctorSchema = new mongoose.Schema({
   name:{
     type: String,
     require: true,
   }
 });
-const Doctor = mongoose.model('Doctor', UserSchema);
+DoctorSchema.plugin(seq.plugin,{
+    inc_field: '_id',
+})
+let Doctor = mongoose.model('Doctor', UserSchema);
 
 
 module.exports = Doctor;
