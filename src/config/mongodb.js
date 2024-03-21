687729b156a189b956f913ebdc7f0fe720018eca
cmhehe176 <example@email.com>
2024-03-21 16:15:16 +0700

    update

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -5,11 +5,19 @@ const Doctor =require('../models/doctor');
 //connect đến mongodb
 let connectDB = async () => {
   try {
+
+
+    //sadfasdf
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Kết nối database thành công!');
+    console.log('Kết nối database thànhlplplpcông!');
   } catch (error) {
     console.error('Lỗi kết nối database:', error);
   }
 };
+
+
+
+
+
 
 module.exports = connectDB;
